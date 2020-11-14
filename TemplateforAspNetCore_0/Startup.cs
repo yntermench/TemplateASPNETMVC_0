2024934cb8416b66cc8e70f702a04eb9dd16a2bc
yntermench28 <yntermench28@mail.ru>
2020-11-14 11:47:51 +0500

    First commit

--- TemplateforAspNetCore_0/Startup.cs
+++ TemplateforAspNetCore_0/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -12,15 +13,18 @@ namespace TemplateforAspNetCore_0
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //добавляем поддержку контроллеров и представлений(MVC)
+            services.AddControllersWithViews()
+                //выставляем совместимость с asp.net core 3.0
+                .SetCompatibilityVersion(CompatibilityVersion.Version_3_0).AddSessionStateTempDataProvider();
         }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            //!!! порядок регистрации middleware очень важен
+
+            //В процессе разработки нам важно видеть подробную информацию об ошибках
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -28,12 +32,13 @@ namespace TemplateforAspNetCore_0
 
             app.UseRouting();
 
+            //подключаем поддержку статичных файлов в приложении (css, js, и т.д.)
+            app.UseStaticFiles();
+
+            //регистрируем нужные нам маршруты (ендпоинты)
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
